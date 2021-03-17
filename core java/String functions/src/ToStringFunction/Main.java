0043d6e987925790c09beaf27aedb5215f030174
saransanjay <62199481+saransanjay@users.noreply.github.com>
2021-03-17 16:31:20 +0530

    Add files via upload

---
+++ core java/String functions/src/ToStringFunction/Main.java
@@ -0,0 +1,11 @@
+package ToStringFunction;
+public class Main{
+	public static void main(String args[]) {
+		ToStringFunction tsf = new ToStringFunction();
+		System.out.println(tsf.toString());
+		System.out.println(tsf.manual());
+		String name ="hi";
+		name.toString();
+		System.out.println(name.toString());
+	}		
+}
---
+++ core java/String functions/src/ToStringFunction/ToStringFunction.java
@@ -0,0 +1,12 @@
+package ToStringFunction;
+
+public class ToStringFunction {
+	private String name ="override";
+	
+	 public String toString() {
+		return name;
+	}
+	 public String manual() {
+		return name;
+	 }
+}
